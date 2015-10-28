c6a23604e182d05ecdb7de26901bdabc329a117a
junior <godsantos@gmail.com>
2015-10-28 02:33:13 -0200

    Comentario

--- src/testeGit1/TerceiraClasse.java
+++ src/testeGit1/TerceiraClasse.java
@@ -3,7 +3,6 @@ package testeGit1;
 public class TerceiraClasse {
 
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("Agora temos um novo pedaço");
 	}
-
 }
