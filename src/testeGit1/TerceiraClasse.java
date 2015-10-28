a9c4e328ef5919f453db75eeeaad8709e7e934bd
godsantos <godsantos@gmail.com>
2015-10-28 02:38:43 -0200

    Update TerceiraClasse.java
    pedaço

--- src/testeGit1/TerceiraClasse.java
+++ src/testeGit1/TerceiraClasse.java
@@ -3,6 +3,6 @@ package testeGit1;
 public class TerceiraClasse {
 
 	public static void main(String[] args) {
-		System.out.println("Agora temos um novo pedaço");
+		System.out.println("Agora temos um novo");
 	}
 }
